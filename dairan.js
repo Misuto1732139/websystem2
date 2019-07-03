73f20d74d66d488561ffd37b3e4167cb315aa3fc
Misuto1732139 <49706264+Misuto1732139@users.noreply.github.com>
2019-07-03 15:51:05 +0900

    Create dairan.js

---
+++ dairan.js
@@ -0,0 +1,26 @@
+const GameName = require('GameName`);
+const Fighter = require('Fighter`);
+const server = GameName();
+const server = Fighter();
+const ejs = require('ejs');
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'node',
+    password: 'websystem',
+    database: 'web'
+});
+
+server.get('/', function( req, res ){
+    connection.query('select id, FighterId, FighterName ',(error,rows,fields)=>{
+        if(error){
+            console.log('Query Error');
+        }
+        res.send('FN.js',{ content: row });
+    });
+});
+
+server.listen(80,function() {
+    console.log('listening on port (');
