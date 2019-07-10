4bb09b75be48af5b324f22d28b99735bf66e511e
Misuto1732139 <49706264+Misuto1732139@users.noreply.github.com>
2019-07-10 15:50:04 +0900

    Update dairan.js

--- dairan.js
+++ dairan.js
@@ -12,7 +12,7 @@ var connection = mysql.createConnection({
 });
 
 server.get('/', function( req, res ){
-    res.status(200).send('mokuzi.ejs',{ title:'Express' });
+    res.render('mokuzi.ejs',{ title:'Express' });
 });
 
 
