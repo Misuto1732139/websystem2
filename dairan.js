da56a1bd147c5dc208ab8ceb3fc7265d462e71f3
Misuto1732139 <49706264+Misuto1732139@users.noreply.github.com>
2019-07-10 16:17:44 +0900

    Update dairan.js

--- dairan.js
+++ dairan.js
@@ -15,7 +15,27 @@ server.get('/', function( req, res ){
     res.render('mokuzi.ejs',{ title:'Express' });
 });
 
+server.get('/games',function( req, res ){
+    let query = 'select GameName.id,Gamename.game_name,buy.number from GameName inner join GameName on GameName.number_id=buy.id ;';
+    console.log( query );
+    connection.query( query, (error, rows, fields) => {
+        if( error ) {
+            console.log('Query Error');
+        }
+        res.render( 'GN.ejs', { content: rows });
+    });
+});
 
+server.get('/characters',function( req, res ){
+    let query = 'select Fighter.id,Fighter.fighter_id,Fighter.fighter_name ;';
+    console.log( query );
+    connection.query( query, (error, rows, fields) => {
+        if( error ) {
+            console.log('Query Error');
+        }
+        res.render( 'FN.ejs', { content: rows });
+    });
+});
     
 server.listen(80,function() {
     console.log('listening on port 80');
