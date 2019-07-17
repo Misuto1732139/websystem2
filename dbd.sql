d952fb3b2f0eabccc14c9cc763f429068edd0d2d
Misuto1732139 <49706264+Misuto1732139@users.noreply.github.com>
2019-07-17 15:05:34 +0900

    Update dbd.sql

--- dbd.sql
+++ dbd.sql
@@ -1,6 +1,6 @@
-create table GameName (id int auto_increment not null primary key, name varchar(100) );
-create table Fighter (id int auto_increment not null primary key, name varchar(100) );
-create table buy (id int auto_increment not null primary key, name varchar(100) );
+create table GameName (id int auto_increment not null primary key, game_name int,  name varchar(100) );
+create table Fighter (id int auto_increment not null primary key, fighter_number int, fighter=name int, name varchar(100) );
+create table buy (id int auto_increment not null primary key, number int);
 load data local infile 'GameName.csv' into table GameName fields terminated by ',' enclosed by '"';
 load data local infile 'Fighter.csv' into table Fighter fields terminated by ',' enclosed by '"';
 load data local infile 'buy.csv' into table buy fields terminated by ',' enclosed by '"';
